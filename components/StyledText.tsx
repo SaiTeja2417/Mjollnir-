746e76da0687da3a3b4d4457492a460727a280dc
@tsai58167 <tsai58167@gmail.com>
2025-09-01 20:02:04 +0530

    Fix: change

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,11 +1,17 @@
-export const Font24Black = ({ children }) => {
+import { ReactNode } from "react";
+
+type FontProps = {
+  children: ReactNode;
+};
+
+export const Font24Black = ({ children } : FontProps) => {
   return (
     <p style={{ color: 'rgb(7, 7, 7)', fontSize: '24px', fontWeight: 600 }}>
       {children}
     </p>
   );
 };
-export const Font22Black = ({ children }) => {
+export const Font22Black = ({ children }: FontProps) => {
   return (
     <p style={{ color: 'rgb(7, 7, 7)', fontSize: '22px', fontWeight: 600 }}>
       {children}
@@ -13,30 +19,30 @@ export const Font22Black = ({ children }) => {
   );
 };
 
-export const Font20Black = ({ children }) => {
+export const Font20Black = ({ children }: FontProps) => {
   return <p style={{ color: 'rgb(7, 7, 7)', fontSize: '20px' }}>{children}</p>;
 };
 
-export const Font18Black = ({ children }) => {
+export const Font18Black = ({ children }: FontProps) => {
   return <p style={{ color: 'rgb(7, 7, 7)', fontSize: '18px' }}>{children}</p>;
 };
 
-export const Font16Black = ({ children }) => {
+export const Font16Black = ({ children }: FontProps) => {
   return <p style={{ color: 'rgb(7, 7, 7)', fontSize: '16px' }}>{children}</p>;
 };
 
-export const Font14Black = ({ children }) => {
+export const Font14Black = ({ children }: FontProps) => {
   return <p style={{ color: 'rgb(7, 7, 7)', fontSize: '14px' }}>{children}</p>;
 };
 
-export const Font24Green = ({ children }) => {
+export const Font24Green = ({ children }: FontProps) => {
   return (
     <p style={{ color: '#00a877', fontSize: '24px', fontWeight: 600 }}>
       {children}
     </p>
   );
 };
-export const Font22Green = ({ children }) => {
+export const Font22Green = ({ children }: FontProps) => {
   return (
     <p style={{ color: '#00a877', fontSize: '22px', fontWeight: 600 }}>
       {children}
@@ -44,19 +50,19 @@ export const Font22Green = ({ children }) => {
   );
 };
 
-export const Font20Green = ({ children }) => {
+export const Font20Green = ({ children }: FontProps) => {
   return <p style={{ color: '#00a877', fontSize: '20px' }}>{children}</p>;
 };
 
-export const Font18Green = ({ children }) => {
+export const Font18Green = ({ children }: FontProps) => {
   return <p style={{ color: '#00a877', fontSize: '18px' }}>{children}</p>;
 };
 
-export const Font16Green = ({ children }) => {
+export const Font16Green = ({ children }: FontProps) => {
   return <p style={{ color: '#00a877', fontSize: '16px' }}>{children}</p>;
 };
 
-export const Font14Green = ({ children }) => {
+export const Font14Green = ({ children }: FontProps) => {
   return <p style={{ color: '#00a877', fontSize: '14px' }}>{children}</p>;
 };
 
